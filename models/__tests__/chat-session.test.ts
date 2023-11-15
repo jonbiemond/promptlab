c34782d41b30752f1b341469a56c2fd47998ae74
Jonathan Biemond <jbiemond@my.bcit.ca>
2023-11-14 20:41:29 -0800

    test: add tests SessionModel and MessageModel
    - Add jest dependency and configure.

---
+++ models/__tests__/chat-session.test.ts
@@ -0,0 +1,29 @@
+import { SessionModel } from '../chat-session';
+import { MessageModel } from '../chat-message';
+
+describe('SessionModel', () => {
+    let session: SessionModel;
+
+    beforeEach(() => {
+        session = new SessionModel();
+    });
+
+    test('addMessage adds a message to the session', () => {
+        const message = new MessageModel('sessionId', 'sender', null, 'text');
+        session.addMessage(message);
+        expect(session.messages).toContain(message);
+    });
+
+    test('updateMessage updates a message in the session', () => {
+        const message = new MessageModel('sessionId', 'sender', null, 'text');
+        session.addMessage(message);
+        message.text = 'updated text';
+        session.updateMessage(message);
+        if (session.messages) {
+            const updatedMessage = session.messages.find(m => m.id === message.id);
+            if (updatedMessage) {
+                expect(updatedMessage.text).toBe('updated text');
+            }
+        }
+    });
+});
