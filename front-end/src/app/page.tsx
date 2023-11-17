bde304dbc90e37ef9bf250554177f30c80bb57fb
derekdbcit <dereksaviodsouza@gmail.com>
2023-11-16 20:55:05 -0800

    Re-setup

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,113 +1,115 @@
+"use client";
+
 import Image from 'next/image'
+import { useState } from 'react';
+import Head from 'next/head';
+import { options } from "./api/auth/[...nextauth]/options"
+import { getServerSession } from "next-auth/next"
 
-export default function Home() {
+type MessageType = {
+  type: 'user' | 'bot';
+  text: string;
+};
+
+type HomeProps = {
+  // Define any props your component may have
+};
+
+// eslint-disable-next-line @next/next/no-async-client-component
+export default function Home(props: HomeProps) {
+
+  /*
+  const session = getServerSession(options)
+  */
+
+  const [messages, setMessages] = useState<MessageType[]>([]);
+  const [userMessage, setUserMessage] = useState<string>('');
+  const [darkMode, setDarkMode] = useState<boolean>(true);
+  const [sidebarVisible, setSidebarVisible] = useState<boolean>(true);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (userMessage.trim() !== '') {
+      setMessages([...messages, { type: 'user', text: userMessage }]);
+      setUserMessage('');
+
+      try {
+        // sending the POST request to the message API endpoint
+        const response = await fetch('/api/message', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ message: userMessage }),
+        });
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok.');
+        }
+
+        // to handle response data from the API
+        const responseData = await response.json();
+        console.log('API response:', responseData);
+        
+        // post the bot's message to the chat
+        setMessages([...messages, { type: 'bot', text: responseData.botResponse }]);
+      } catch (error) {
+        console.error('Error sending message:', error);
+        // handle errors, show user-friendly messages, etc.
+      }
+    }
+  };
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className={`container ${darkMode ? 'bg-black text-white' : 'bg-white text-black'}`}>
+      <Head>
+        <title>Conscious Learning</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className="p-4 flex justify-between items-center">
+        <button onClick={toggleDarkMode} className="bg-gray-700 text-white px-4 py-2 rounded cursor-pointer">
+          Toggle Dark Mode
+        </button>
+        <a className="px-4">Goals</a>
+        <a className="px-4">Logout</a>
+      </header>
+
+      <div className="wrapper">
+        <main className="main-content flex-1 p-4">
+          <div className={`chat ${sidebarVisible ? 'with-sidebar' : ''}`}>
+            {messages.map((message, index) => (
+              <div key={index} className={message.type === 'user' ? 'userMessage' : 'botMessage'}>
+                <p className="text-left">{message.text}</p>
+              </div>
+            ))}
+            <form onSubmit={handleSubmit} className="p-4">
+              <input
+                type="text"
+                id="request"
+                value={userMessage}
+                onChange={(e) => setUserMessage(e.target.value)}
+                placeholder="Send a message"
+                className="p-2 border border-gray-400 rounded mr-2"
+              />
+              <input
+                type="submit"
+                value="Send"
+                className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"
+              />
+            </form>
+          </div>
+        </main>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <footer className="p-4 flex justify-center items-center border-t border-gray-300">
+        Powered by ChatGPT
+      </footer>
+    </div>
+  );
 }
