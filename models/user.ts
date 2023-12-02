d35924e2f7e11a41acbc49c5c8bf7b3388c8d216
Jonathan Biemond <jbiemond@my.bcit.ca>
2023-12-02 10:40:32 -0800

    feat(db): add user model

---
+++ models/user.ts
@@ -0,0 +1,46 @@
+import { v4 as uuidv4 } from 'uuid';
+import { Dao } from "./dao";
+
+interface User {
+    id: string;
+    type: string;
+    userId: string;
+    username: string;
+    password_hash: string;
+    recentSessions?: string[];
+}
+
+class UserModel extends Dao<UserModel> implements User {
+    public id: string;
+    public type: string;
+    public userId: string;
+    public username: string;
+    public password_hash: string;
+    public recentSessions?: string[];
+
+    constructor(username: string, password_hash: string) {
+        super();
+        this.id = uuidv4();
+        this.type = 'User';
+        this.userId = this.id;
+        this.username = username;
+        this.password_hash = password_hash;
+        this.recentSessions = [];
+    }
+
+    protected get containerId(): string {
+        return 'user';
+    }
+
+    public addSession(sessionId: string): void {
+        if (!this.recentSessions) {
+            this.recentSessions = [];
+        }
+        this.recentSessions.push(sessionId);
+        if (this.recentSessions.length > 10) {
+            this.recentSessions.shift();
+        }
+    }
+}
+
+export { UserModel };    export type { User };
